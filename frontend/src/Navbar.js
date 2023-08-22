5b90aa6fb4d5c0820ff24d91ff16ef8844ac536b
MichaelShingo <mcrawford5376@gmail.com>
2023-08-22 22:47:34 +0200

    responsive navgar in progress

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,6 +3,8 @@ import urls from './services/urls';
 import { logout, checkAuthenticated } from './services/authService';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import ThemeToggle from './components/ThemeToggle';
+import { ReactComponent as Hamburger } from './svg/hamburger.svg';
+
 import './styles/navbar.css';
 
 const Navbar = ( {toggleMode} ) => {
@@ -10,10 +12,26 @@ const Navbar = ( {toggleMode} ) => {
     const location = useLocation();
     const [url, setUrl] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    let displayValue;
+
+    if (screenWidth > 815) {
+        displayValue = 'flex';
+    } else {
+        displayValue = showMenu ? 'flex' : 'none';
+    }
+
+    const handleResize = () => {
+        setScreenWidth(window.innerWidth);
+    }
 
     useEffect(() => {
         setIsAuthenticated(checkAuthenticated());
-    }, )
+
+        window.addEventListener('resize', handleResize);
+    }, []); // should this actually have an empty dependency array? 
 
     useEffect(() => {
         setUrl(location.pathname);
@@ -26,18 +44,28 @@ const Navbar = ( {toggleMode} ) => {
         setIsAuthenticated(checkAuthenticated());
         console.log(checkAuthenticated());
     }
+
+    const handleHamburgerClick = () => {
+        setShowMenu(prev => !prev);
+        console.log('show menu');
+    }
     
     return ( 
         <div className="navbar row">
             <div className="col-1"></div>
-
             <div className="col-10 nav-content">
                 <div className="row">
                     <div className="col-3">
                         <h1>Violintice</h1>
                     </div>
-                    <div className="col-3"></div>
-                    <div className="col-6 links">
+                    <div className="col-3 nav-bar-middle" ></div>
+                    
+                    <div 
+                        className="col-6 links"
+                        style={{
+                            top: displayValue === 'flex' ? '0%' : '-500%'
+                        }}
+                    >
                         <ThemeToggle 
                             toggleMode={toggleMode}
                         />
@@ -53,12 +81,19 @@ const Navbar = ( {toggleMode} ) => {
                         }
                     </div>
                 </div>
+                <div 
+                    className="hamburger"
+                    onClick={handleHamburgerClick}
+                >
+                    <div className="hamburger-line-container">
+                        <div className="hamburger-line"></div>
+                        <div className="hamburger-line"></div>
+                        <div className="hamburger-line"></div>
+                    </div>
+                </div>
             </div>
             <div className="col-1"></div>
-        </div>
-            
-
-            
+        </div>      
      );
 }
  
